af5aeaf0bbda134d7d677692fdd5938faf1dcc0c
Jacob Parrish <jacob@upgradeya.com>
2021-07-21 21:05:32 -0400

    Add redirect aliases.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,8 @@ require('bootstrap')
 // Syntax highlighting for code.
 // var hljs = require('highlightjs')
 // hljs.initHighlightingOnLoad();
+
+// Redirects for qr codes and links so we can track marketing.
+var pathname = window.location.pathname; // Returns path only (/path/example.html)
+pathname === '/qr-sticker1' ? window.location.replace('https://bitcoincharlotte.org/') : '';
+pathname === '/qr-foam-board1' ? window.location.replace('https://bitcoincharlotte.org/') : '';
