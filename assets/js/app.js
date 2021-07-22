566ab35fbea18f01976424a734ff95c0b26b2f9c
Jacob Parrish <jacob@upgradeya.com>
2021-07-21 22:05:51 -0400

    fix the redirects so the analytics can see them, hopefully!

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,5 +9,5 @@ require('bootstrap')
 
 // Redirects for qr codes and links so we can track marketing.
 var pathname = window.location.pathname; // Returns path only (/path/example.html)
-pathname === '/qr-sticker1' ? window.location.replace('https://bitcoincharlotte.org/') : '';
-pathname === '/qr-foam-board1' ? window.location.replace('https://bitcoincharlotte.org/') : '';
+pathname === '/qr-sticker1' ? window.location('https://bitcoincharlotte.org/') : '';
+pathname === '/qr-foam-board1' ? window.location('https://bitcoincharlotte.org/') : '';
