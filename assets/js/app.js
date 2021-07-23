6fdfaece1367b704e63861c0657c8b4557aaeb19
Jacob Parrish <jacob@upgradeya.com>
2021-07-23 00:54:15 -0400

    Fix autoplaying video embeds. Add new post - jack mallers and strike. Add redirect for qr code flier1.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,6 @@ require('bootstrap')
 // Redirects for qr codes and links so we can track marketing.
 var pathname = window.location.pathname; // Returns path only (/path/example.html)
 window.onload = function() {
-	pathname === '/qr-sticker1' ? window.location.href = '/' : '';
+	pathname === '/qr-event-flier1' ? window.location.href = '/events' : '';
 	pathname === '/qr-foam-board1' ? window.location.href = '/' : '';
 }
