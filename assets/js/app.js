4cfe4661f2b45ad53b6ddda46a91a2a4000bcf3d
Jacob Parrish <jacob@upgradeya.com>
2021-08-19 17:10:58 -0400

    Add new brief and update events page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,8 @@ require('bootstrap')
 // Redirects for qr codes and links so we can track marketing.
 var pathname = window.location.pathname; // Returns path only (/path/example.html)
 window.onload = function() {
-	pathname === '/qr-event-flier1' ? window.location.href = '/events' : '';
-	pathname === '/qr-event-flier2' ? window.location.href = '/events' : '';
-	pathname === '/qr-foam-board1' ? window.location.href = '/' : '';
-	pathname === '/qr-dot-org' ? window.location.href = '/about' : '';
+	pathname === '/qr-event-flier1' ? window.location.href = '/events' : ''; // Why Kids Need Bitcoin. Isaiah Jackson
+	pathname === '/qr-event-flier2' ? window.location.href = '/events' : ''; // What is Money? David Seroy Flier
+	pathname === '/qr-foam-board1' ? window.location.href = '/' : ''; // Bitcoin Charlotte foam baords
+	pathname === '/qr-dot-org' ? window.location.href = '/about' : ''; // 
 }
